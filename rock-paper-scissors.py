e28cf402fba6d6ea6c7f3cfdcd0c037b5a046071
Eoin Reid <40269943+EoinReid@users.noreply.github.com>
2022-11-04 20:24:26 +0000

    adds move functions & try/except block

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -10,38 +10,56 @@ user_wins = 0
 user_loses = 0
 user_draws = 0
 
-while user_input != "q": # as long as the user does not enter q
-    print("ROCK, PAPER, SCISSORS..")
-    print(str(user_wins) + " wins. " + str(user_loses) + " loses. " + str(user_draws) + " draws.") # scoreboard
-    user_input = input("Enter a move: (r)ock (p)aper (s)cissors or (q)uit ")
-    # create a dictionary that creates a key for each of the 4 user input possilities r, p, s and q so I can take the letter the user input and take a value based on it. e.g r = rock
-    # also randomly generting an int 1 - 3 and assigning a value from this dictionary to randomly generate the computers move.
+# functions
+def user_make_a_move(user_input):
     move = { 
         "r":"rock",
         "p":"paper",
         "s":"scissors",
         "q":"quit",
+        }
+    user_move = move[user_input]
+    return user_move
+
+def computer_make_a_move():
+    move = { 
         "1":"rock",
         "2":"paper",
         "3": "scissors"
     }
-    user_move = move[user_input]
     computer_move = move[str(random.randint(1, 3))]
+    return computer_move
+
+# Rock-Paper-Scissors application
+
+try:
+    while user_input != "q": # as long as the user does not enter q
+        print("ROCK, PAPER, SCISSORS..")
+        print(str(user_wins) + " wins. " + str(user_loses) + " loses. " + str(user_draws) + " draws.") # scoreboard
+        user_input = input("Enter a move: (r)ock (p)aper (s)cissors or (q)uit ")
+        # create a dictionary that creates a key for each of the 4 user input possilities r, p, s and q so I can take the letter the user input and take a value based on it. e.g r = rock
+        # also randomly generting an int 1 - 3 and assigning a value from this dictionary to randomly generate the computers move.
     
-    if user_move == "quit":
-        sys.exit()
-    elif user_move == "rock" and computer_move == "scissors":
-        print("You win! Rock beats scissors!")
-        user_wins += 1
-    elif user_move == "paper" and computer_move == "rock":
-        print("You win! Paper beats scissors!")
-        user_wins += 1
-    elif user_move == "scissors" and computer_move == "paper":
-        print("You win! Scissors beats papers!")
-        user_wins += 1
-    elif user_move == computer_move:
-        print("Aw no, its a draw!")
-        user_draws += 1
-    else:
-        print("RIP, you lose.")
-        user_loses += 1
+        user_move = user_make_a_move(user_input)
+        computer_move = computer_make_a_move()
+        
+        if user_move == "quit":
+            sys.exit()
+        elif user_move == "rock" and computer_move == "scissors":
+            print("You win! Rock beats scissors!")
+            user_wins += 1
+        elif user_move == "paper" and computer_move == "rock":
+            print("You win! Paper beats scissors!")
+            user_wins += 1
+        elif user_move == "scissors" and computer_move == "paper":
+            print("You win! Scissors beats papers!")
+            user_wins += 1
+        elif user_move == computer_move:
+            print("Aw no, its a draw!")
+            user_draws += 1
+        else:
+            print("RIP, you lose.")
+            user_loses += 1
+except:
+    print("An error has occured, please try again.")
+
